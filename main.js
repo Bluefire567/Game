9e491e6bace2958952a98d474dadbb291ecbc8b0
vickichen22 <vickichen000222@gmail.com>
2017-07-26 14:43:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -46,8 +46,42 @@ var mainState = {
 
     },
 
-    update: function() {
-
+    update: function() {        
+        if (cursors.left.isDown)
+            {
+                //  Move to the left
+               player.body.velocity.x = -150;
+               player.body.velocity.y = 0;
+               this.game.camera.x -= 1.25;
+               player.animations.play('left');
+            }
+        else if (cursors.right.isDown)
+            {
+               //  Move to the right
+               player.body.velocity.x = 150;
+               player.body.velocity.y = 0;
+               this.game.camera.x += 1.25;
+               player.animations.play('right');
+            }
+         else if(cursors.up.isDown){
+               player.body.velocity.y = -150;
+               player.body.velocity.x = 0;
+               player.animations.play("up");
+            }
+         else if(cursors.down.isDown){
+               player.body.velocity.y = 150;
+               player.body.velocity.x = 0;
+               player.animations.play("down");
+            }
+         else
+            {
+                //  Stand still
+               player.animations.stop();
+                
+               player.body.velocity.x = 0;
+               player.body.velocity.y = 0;
+            }
+  
     },
 
     render: function() {
