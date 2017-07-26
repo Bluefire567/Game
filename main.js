b5314def7c46683066381b72839560b063da0002
vickichen22 <vickichen000222@gmail.com>
2017-07-26 14:03:10 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 var game = new Phaser.Game(800,600,Phaser.CANVAS,'game.Div')
+    //var bg;
+var player;
+var cursors;
+var facing = 'left';
+var look = 'up';
+var house;
+var tree;
+
 var mainState = {
     preload: function() {
-        this.load.image('background', 'background.png');
-        this.game.load.spritesheet("dog","dog.png", 42, 40);
-        this.game.load.spritesheet("car", "car.png", 52, 49);
-        this.game.load.image("house", "house.png", 65, 60);
-        this.game.load.image("otherdog", "dogimage.png", 42, 40);
-        this.game.load.image("tree", "tree.png", 75, 70);
+        this.load.image('background', 'https://s3.amazonaws.com/spoonflower/public/design_thumbnails/0424/5908/1431605648965_drop_preview.png');
+        this.game.load.spritesheet("dog","Game/dog.png", 42, 40);
+        this.game.load.spritesheet("car", "Game/car.png", 52, 49);
+        this.game.load.image("house", "Game/house.png", 65, 60);
+        this.game.load.image("otherdog", "Game/dogimage.png", 42, 40);
+        this.game.load.image("tree", "Game/tree.png", 75, 70);
     
     },
-
-    //var bg;
-    var player;
-    var cursors;
-    var facing = 'left';
-    var look = 'up';
-    var house;
-    var tree;
 
     create: function() {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -27,7 +27,7 @@ var mainState = {
         player = game.add.sprite(0 , 100, 'dog');
         this.game.physics.enable(player, Phaser.Physics.ARCADE);
 
-        Player.body.bounce.y = 0.2;
+        player.body.bounce.y = 0.2;
         player.body.collideWorldBounds = true;
     
         this.game.physics.arcade.gravity.y = 250;
@@ -41,58 +41,93 @@ var mainState = {
 
         cursors = this.game.input.keyboard.createCursorKeys();
 
-    //tree = this.game.add.sprite( , , “tree”);
-    //tree.body.immovable = true;
-    //house = this.game.add.sprite( , , “house”);
-    //house.body.immovable = true;
-    //otherdog = this.game.add.sprite( , , “otherdog”);
-    
+        //tree = this.game.add.sprite( , , “tree”);
+        //tree.body.immovable = true;
+        //house = this.game.add.sprite( , , “house”);
+        //house.body.immovable = true;
+        //otherdog = this.game.add.sprite( , , “otherdog”);
     
 
     },
 
     update: function() {
-        {player.body.velocity.x = 0
-
         if (cursors.left.isDown)
-        { 
-            player.body.velocity.x = -150;
-
-            if (facing != 'right')
             {
+                //  Move to the left
+                player.body.velocity.x = -150;
+                player.body.velocity.y = 0;
+                this.game.camera.x -= 1.25;
                 player.animations.play('left');
-                facing = 'left';
-        }
-
-        },
-        else if (cursors.right.isDown)
-        {
-            player.body.velocity.x = 150;
-
-            if (facing != 'right')
-                { 
-                player.animations.play('right');
-                facing = 'right';
             }
-        }
-        else
-        {
-            if (facing != 'idle')
+            else if (cursors.right.isDown)
             {
-                player.animations.stop();
-
-                if (facing == 'left')
-                {
-                    player.frame = 5;
-                }
-                else
-                {
-                    player.frame = 5;
-                }
-
-                facing = 'idle';
+                //  Move to the right
+                player.body.velocity.x = 150;
+                player.body.velocity.y = 0;
+                this.game.camera.x += 1.25;
+                player.animations.play('right');
             }
-        }
+            else if(cursors.up.isDown){
+                player.body.velocity.y = -150;
+                player.body.velocity.x = 0;
+                player.animations.play("up");
+            }
+            else if(cursors.down.isDown){
+                player.body.velocity.y = 150;
+                player.body.velocity.x = 0;
+                player.animations.play("down");
+            }
+            else
+            {
+                //  Stand still
+                player.animations.stop();
+                
+                player.body.velocity.x = 0;
+                player.body.velocity.y = 0;
+            }
+    }
+
+        //{player.body.velocity.x = 0
+
+        //if (cursors.left.isDown)
+        //{ 
+            //player.body.velocity.x = -150;
+
+            //if (facing != 'right')
+            //{
+               //player.animations.play('left');
+                //facing = 'left';
+            //}
+
+        //},
+        //else if (cursors.right.isDown)
+        //{
+            //player.body.velocity.x = 150;
+
+            //if (facing != 'right')
+                //{ 
+                //player.animations.play('right');
+                //facing = 'right';
+            //}
+        //}
+        //else
+        //{
+            //if (facing != 'idle')
+            //{
+                //player.animations.stop();
+
+                //if (facing == 'left')
+                //{
+                    //player.frame = 5;
+                //}
+                //else
+                //{
+                    //player.frame = 5;
+                //}
+
+                //facing = 'idle';
+            //}
+        //}
 
     render: function() {
 
