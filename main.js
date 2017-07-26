2c5ce5ab633caaf613c92ac15c10e379ff440a8e
Bluefire567 <Bonniellhwhite@gmail.com>
2017-07-26 15:02:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -14,26 +14,33 @@ var mainState = {
         game.load.image('house', 'house.png');
         game.load.image('otherdog', 'dogimage.png', 42, 40);
         game.load.image('tree', 'tree.png', 75, 70);
+        game.load.image('dogleft','dogRight.png');
+        game.load.image('dogright','dogRight.png');
+        game.load.image('redcardown','carRed(down)');
+        game.load.image('redcarup','carRed(up)');
+        
     },
 
 
     create: function() {
         game.load.start();
-        //game.physics.startSystem(Phaser.Physics.ARCADE);
         game.world.setBounds(0, 0, 900, 600);
         background = game.add.sprite(0, 0, 'background');
         player = game.add.sprite(0 , 100, 'dog');
-        //game.physics.enable(player, Phaser.Physics.ARCADE)
+        playerleft = game.add.sprite(200, 400, 'dogleft');
+        playerright = game.add.sprite(200, 500, 'dogright');
+        cardown = game.add.sprite(0,200,'redcardown');
+        carup  = game.add.sprite(0,400, 'redcarup');
         tree1 = game.add.sprite( 200, 300, 'tree');
         house1 = game.add.sprite( 200, 200, 'house');
-        otherdog1 = game.add.sprite( 200, 400, 'otherdog');
+        //otherdog1 = game.add.sprite( 200, 400, 'otherdog');
         tree.body.immovable = true;
         
         player.body.bounce.y = 0.2;
         player.body.collideWorldBounds = true;
         cursors = game.input.keyboard.createCursorKeys();
-
-
+        //game.physics.startSystem(Phaser.Physics.ARCADE);
+        //game.physics.enable(player, Phaser.Physics.ARCADE)
         //game.physics.arcade.gravity.y = 250;
 
         player.animations.add('left', [13, 14, 15, 16], 13, true);
