512340644f64832940ca17056f64387d073c73ea
Bluefire567 <Bonniellhwhite@gmail.com>
2017-07-31 15:58:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -33,27 +33,22 @@ var mainState = {
     },
 
     create: function() {
-        //Setup
+//Setup
         game.load.start();
         game.world.setBounds(0, 0, 2000, 1200);
         game.physics.startSystem(Phaser.Physics.arcade);
-
- //Background
+//Background
         background = game.add.tileSprite(0,0,2000,1200,'background');
         background = fixedToCamara = true;
-
+//Text
         style = { font: "50px Arial", fill: "#ff0044", align: "center" };
         text = game.add.text(game.world.centerX, game.world.centerY, "HELLO",style)
         text.fixedToCamera = true;
         text.cameraOffset.setTo(0,550);
-        
 
-       
-
-        //Main sprite
+//Main sprite
         playerdown = game.add.sprite(0 , 100, 'dogdown');
-
-        //Player movement
+//Player movement
         game.physics.enable(playerdown , Phaser.Physics.ARCADE);
         game.camera.follow(playerdown);
         playerdown.body.collideWorldBounds = true;
@@ -63,53 +58,46 @@ var mainState = {
         playerdown.animations.add('walkright',[4,5,6,7], 10, true);
         playerdown.animations.add('walkup',[8,9,10,11], 10, true);
         playerdown.animations.add('walkleft',[12,13,14,15], 10, true);
-       
+//Obstacles
         tree1 = game.add.sprite( 260, 140, 'tree');
-        //Sprites  
         redcar = game.add.sprite(0,200,'redcardown');
-
-        //Housesprite
         house1 = game.add.sprite( 400, 0, 'house');
-        game.physics.enable(playerdown , Phaser.Physics.ARCADE);
-        house1.body.enable = true; 
-        house1.immovable = true;
-        
-
+//Obstacle Settings
+        game.physics.enable([playerdown,house1, tree1, redcar], Phaser.Physics.ARCADE);
+        redcar.body.immovable = true;
+        house1.body.immovable = true; 
     },
 
     update: function() {
+        //game.physics.arcade.collide(playerdown,house1)
+        //game.physics.arcade.collide(playerdown,tree1)
     //Movement
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
-        playerdown.x -= 4;
+        playerdown.body.velocity.x = -4;
         playerdown.animations.play('walkleft');
         playerdown.animations.stop = false;
     }
 
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
-        playerdown.x += 4;
+        playerdown.body.velocity.x = 4;
         playerdown.animations.play('walkright');
         playerdown.animations.stop = false;
     }
         if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
     {
-        playerdown.y += 4;
+        playerdown.body.velocity.body.y = 4;
         playerdown.animations.play('walkdown');
         playerdown.animations.stop = false;
     }
 
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
     {
-        playerdown.y -= 4;
+        playerdown.body.velocity.y = -1;
         playerdown.animations.play('walkup');
         playerdown.animations.stop = false;  
     }
-
-    //if game.physics.arcade.collide(playerdown, house1)
-    //   {
-    //   text = game.add.text(game.world.centerX, game.world.centerY, "You are at a house",style)
-    //   }    
 
 
     },
